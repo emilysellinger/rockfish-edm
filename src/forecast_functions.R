dec8fdfb401881a89fbf4a5a8fb9540d7775929e
Emily Sellinger <71989576+emilysellinger@users.noreply.github.com>
2022-05-03 16:09:07 -0700

    Update forecast_functions.R

--- src/forecast_functions.R
+++ src/forecast_functions.R
@@ -5,12 +5,24 @@ plot(rec_ts$y, type = "b")
 # Functions ---------------------------------------------------------------
 # going to first define the forecasting functions
 
-rec_mean <- function(x, ts){
-  df <- ts[1:x, 1]
-  mu <- mean(ts)
-  sigmaR <- sd(ts)
+rec_mean <- function(X){
+  df <- rec_ts[1:X, 1]
+  mu <- mean(df)
+  sigmaR <- sd(df)
   
+  pred <- mu + rnorm(1, 0, sigmaR)
+  
+  return(pred)
 }
 
+test <- lapply(seq(40, length(rec_ts$y),1), rec_mean())
 
-rec_mean <- lm(y~1, data = rec_ts)
+
+# Example code
+X1 <- runif(50, 0, 1)
+X2 <- runif(50, 0, 10) # I included another variable just for a better demonstration
+Y <- runif(50, 0, 1)
+df <- data.frame(X1,X2,Y)
+
+
+rolling_lms <- lapply( seq(20,nrow(df) ), function(x) lm( Y ~ X1+X2, data = df[1:x , ]) )
