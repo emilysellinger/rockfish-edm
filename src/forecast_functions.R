6b2edf843c432a05fb88f960abd614a7aacdef65
Emily Sellinger <71989576+emilysellinger@users.noreply.github.com>
2022-05-04 16:48:49 -0700

    Update forecast_functions.R

--- src/forecast_functions.R
+++ src/forecast_functions.R
@@ -1,28 +1,70 @@
+library(stats4)
+spawn_ts <- data.frame(y = runif(60, 50, 150))
+rec_ts <- 0.8*spawn_ts/(1 + 1.2*spawn_ts)
 
-rec_ts <- data.frame(y = rnorm(60, 100, 10))
+
 plot(rec_ts$y, type = "b")
 
 # Functions ---------------------------------------------------------------
 # going to first define the forecasting functions
 
-rec_mean <- function(X){
-  df <- rec_ts[1:X, 1]
-  mu <- mean(df)
-  sigmaR <- sd(df)
+rec_mean <- function(x,df){
+  # subset data to window size
+  dat <- df[1:x, 1]
   
+  # calculate mean and standard deviation of time series window
+  mu <- mean(dat)
+  sigmaR <- sd(dat)
+  
+  # calculate prediction
   pred <- mu + rnorm(1, 0, sigmaR)
+  return(pred)
+
+}
+
+rec_AR <- function(x, df){
+  # subset data to window size
+  dat <- df[1:x, 1]
   
+  # fit AR model and calculate standard deviation (atm doing separately)
+  mod <- arima0(dat, order = c(1,0,0))
+  sigmaR <- sd(dat)
+  
+  one_ahead <- predict(mod, n.head = 1)
+  pred <- one_ahead$pred[1] + rnorm(1, 0, sigmaR)
   return(pred)
 }
 
-test <- lapply(seq(40, length(rec_ts$y),1), rec_mean())
+
+BHminusLL <- function(loga, logb, logsigmaR){
+  # extract parameters
+  a <- exp(loga); b <- exp(logb); sigma <- exp(logsigmaR)
+  # make predictions
+  pred <- a*dats/(1 + b*dats)
+  
+  #calculate negative log like
+  NegLogL <- (-1)*sum(dnorm(datr, pred, sigmaR, log = TRUE))
+  return(NegLogL)
+}
+
+rec_BH <- function(x, df1, df2){
+  # subset spawning and recruit data to window size
+  dats <- df1[1:x, 1]
+  datr <- df2[1:x, 1]
+  
+  starts <- list(loga = log(0.6), logb = log(1), logsigmaR = 1)
+  mle_out <- mle(BHminusLL, start = starts)
+  
+  return(mle_out)
+}
+
+# Predictions -------------------------------------------------------------
+test <- seq(40, length(rec_ts$y),1)
+rec_preds <- rep(NA, length(test))
+
+for(i in 1:length(test)){
+  rec_preds[i] <- rec_AR(test[i], rec_ts)
+}
 
 
-# Example code
-X1 <- runif(50, 0, 1)
-X2 <- runif(50, 0, 10) # I included another variable just for a better demonstration
-Y <- runif(50, 0, 1)
-df <- data.frame(X1,X2,Y)
 
-
-rolling_lms <- lapply( seq(20,nrow(df) ), function(x) lm( Y ~ X1+X2, data = df[1:x , ]) )
