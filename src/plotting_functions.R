6ad7a03ca807221ec671c8b08b10be5c087bb880
Emily Sellinger <71989576+emilysellinger@users.noreply.github.com>
2022-12-02 11:48:27 -0800

    Create plotting_functions.R

---
+++ src/plotting_functions.R
@@ -0,0 +1,8 @@
+coverage_prob_plot <- function(preds,stock_df){
+  fmethod <- dim(preds)[3]
+  methods <- c("mean", "AR(1)", "Beverton-Holt", "simplex projection", "HMM")
+  
+  for(i in 1:fmethod){
+    
+  }
+}
