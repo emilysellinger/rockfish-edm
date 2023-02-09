075498ea6045d85f5c8de8e2088f457d13462582
Emily Sellinger <71989576+emilysellinger@users.noreply.github.com>
2023-02-09 14:35:12 -0800

    put updated functions into original script

--- src/forecast_functions.R
+++ src/forecast_functions.R
@@ -6,16 +6,17 @@ library(NatParksPalettes)
 library(here)
 library(tidyverse)
 library(dplyr)
-library(truncnorm)
 library(rEDM)
 library(gridExtra)
 library(zoo)
 library(grid)
 library(changepoint)
+library(patchwork)
 # Short-term Forecast Functions ---------------------------------------------------------------
-# x = test set vector, df1 = recruitment time series, df2 = spawning biomass time series
+# x = test set vector, df1 = recruitment time series, df2 = spawning biomass time series, 
+# nsims = number of simulations
 
-rec_mean <- function(x, df1){
+rec_mean <- function(x, df1, nsims){
   # subset data to window size
   dat <- df1[1:(x-1)]
   
@@ -24,13 +25,13 @@ rec_mean <- function(x, df1){
   #print(mu)
   sigmaR <- sd(log(dat))
   
-  # calculate prediction
-  pred <- exp(mu + rnorm(1, 0, sigmaR))
+  # calculate predictions for each simulation
+  pred <- exp(mu + rnorm(nsims, 0, sigmaR))
   return(pred)
-
+  
 }
 
-rec_AR <- function(x, df1){
+rec_AR <- function(x, df1, nsims){
   
   # subset data to window size
   dat <- df1[1:(x-1)]
@@ -40,11 +41,11 @@ rec_AR <- function(x, df1){
   sigmaR <- sqrt(mod$sigma2)
   
   # calculate prediction
-  pred <- exp((forecast(mod, h = 1)$mean)[1] + rnorm(1, 0, sigmaR))
+  pred <- exp((forecast(mod, h = 1)$mean)[1] + rnorm(nsims, 0, sigmaR))
   return(pred)
 }
 
-rec_BH <- function(x, df1, df2){
+rec_BH <- function(x, df1, df2, nsims){
   # subset spawning and recruit data to window size
   datr <- df1[1:(x-1)]
   dats <- df2[1:(x-1)]
@@ -73,105 +74,12 @@ rec_BH <- function(x, df1, df2){
   sigmaR <- exp(coef(mle_out)[3])
   
   # predict one step ahead
-  one_ahead <- a*s_x/(1 + b*s_x)*exp(rnorm(1,0,sigmaR))
+  one_ahead <- a*s_x/(1 + b*s_x)*exp(rnorm(nsims,0,sigmaR))
   
   return(unname(one_ahead))
 }
 
-# functions for sampling method
-run.pred.mc.sim <- function(P, num.iters = 2, final_state){
-  
-  # number of possible states
-  num.states <- nrow(P)
-  # create vector for states
-  states <- rep(NA, num.iters)
-  
-  # initialize variable for first state 
-  states[1] <- final_state
-  
-  for(t in 2:num.iters) {
-    
-    # probability vector to simulate next state X_{t+1}
-    p  <- P[states[t-1], ]
-    
-    ## draw from multinomial and determine state
-    states[t] <-  which(rmultinom(1, 1, p) == 1)
-  }
-  return(states)
-}
-# sampling method recruitment forecast
-pred.rec <- function(future_states, mu1, mu2, sd1, sd2){
-  preds <- rep(NA, length(future_states))
-  
-  for(i in 1:length(future_states)){
-    if(future_states[i] == 1){
-      preds[i] <- rtruncnorm(1, a = 10, b= Inf, mu1, sd1)
-    }else{
-      preds[i] <- rtruncnorm(1, a = 10, b = Inf, mu2, sd2)
-    }
-  }
-  
-  return(preds)
-}
-
-rec_HMM_sample <- function(x, df1, df2){
-  # subset spawning and recruit data to window size
-  datr <- df1[1:(x-1)]
-  dats <- df2[1:(x-1)]
-  
-  # make a data frame for depmix package
-  a <- tibble(rec = datr,
-              spawn = dats,
-              logRS = log(datr/dats),
-              logR = log(datr))
-  # fit hidden markov model
-  mod1 <- depmix(logRS ~ spawn, data = a, nstates = 2, family = gaussian())
-  fit_mod1 <- fit(mod1, em = em.control(maxit = 500))
-
-  mod2 <- depmix(logRS ~ spawn, data = a, nstates = 1, family = gaussian())
-  fit_mod2 <- fit(mod2)
-  
-  if(AIC(fit_mod2) < AIC(fit_mod1)){
-    print("one state best")
-    mu1 <- mean(a$rec)
-    sd1 <- sd(a$spawn)
-    pred <- rtruncnorm(1, a = 10, b= Inf, mu1, sd1)
-    
-    # return forecasts
-    return(pred)
-    
-  }else{
-    print("two state best")
-    fit_post <- posterior(fit_mod1)
-    
-    # update data frame with posterior state classification
-    a <- a %>% 
-      add_column(est_state = fit_post$state)
-    # Filter dataframe by state
-    est_state_1 <- a %>% 
-      filter(est_state == 1)
-    est_state_2 <- a %>% 
-      filter(est_state == 2)
-    
-    # Determine the estimated state at the last time step
-    final_state <- pull(a[nrow(a), "est_state"])
-    
-    # Extract estimated transition matrix
-    est_P <- t(matrix(getpars(fit_mod1)[3:6], nrow = 2, ncol = 2))
-    
-    # predict states for forecast years
-    future_states <- run.pred.mc.sim(est_P, num.iters = 2, final_state)
-    
-    
-    rec_preds <- pred.rec(future_states, mu1 = mean(est_state_1$rec), mu2 = mean(est_state_2$rec),
-                          sd1 = sd(est_state_1$rec), sd2 = sd(est_state_2$rec))
-    # return forecasts
-    return(rec_preds[2])
-    
-  }
-}
-
-rec_simplex <- function(x, df1){
+rec_simplex <- function(x, df1, nsims){
   
   sim_lib <- c(1, x-1)
   # limit on updating E, simplex needs at least 2 data points to predict
@@ -198,13 +106,117 @@ rec_simplex <- function(x, df1){
   preds <- na.omit(simplex_output2$model_output[[1]])
   
   # return forecast
-  pred <- exp(preds[1, 3] + rnorm(1, 0, sigmaR))
+  pred <- exp(preds[1, 3] + rnorm(nsims, 0, sigmaR))
   return(pred)
 }
-# Long-term forecast functions -------------------------------------------------------------
-lrec_mean <- function(x, df1){
+
+# functions for sampling method
+run.pred.mc.sim <- function(P, num.iters = 2, final_state){
+  
+  # number of possible states
+  num.states <- nrow(P)
+  # create vector for states
+  states <- rep(NA, num.iters)
+  
+  # initialize variable for first state 
+  states[1] <- final_state
+  
+  for(t in 2:num.iters) {
+    
+    # probability vector to simulate next state X_{t+1}
+    p  <- P[states[t-1], ]
+    
+    ## draw from multinomial and determine state
+    states[t] <-  which(rmultinom(1, 1, p) == 1)
+  }
+  return(states)
+}
+# sampling method recruitment forecast
+pred.rec <- function(future_state, mu1, mu2, sd1, sd2, df2, x){
+  
+  if(future_state == 1){
+    logRS <- rnorm(1,mu1,sd1)
+    pred <- exp(logRS + log(df2[x]))
+  }else{
+    logRS <- rnorm(1, mu2, sd2)
+    pred <- exp(logRS + log(df2[x]))
+  }
+  
+  return(pred)
+}
+
+rec_HMM_sample <- function(x, df1, df2, nsims){
+  # subset spawning and recruit data to window size
+  datr <- df1[1:(x-1)]
+  dats <- df2[1:(x-1)]
+  
+  # make a data frame for depmix package
+  a <- tibble(rec = datr,
+              spawn = dats,
+              logRS = log(datr/dats),
+              logR = log(datr))
+  # fit hidden markov model
+  mod1 <- depmix(logRS ~ spawn, data = a, nstates = 2, family = gaussian(), instart = c(0.9,0.1))
+  fit_mod1 <- fit(mod1, em = em.control(maxit = 500))
+  
+  
+  fit_post <- posterior(fit_mod1, type = "viterbi")
+  
+  # update data frame with posterior state classification
+  a <- a %>% 
+    add_column(est_state = fit_post$state)
+  # Filter dataframe by state
+  est_state_1 <- a %>% 
+    filter(est_state == 1)
+  est_state_2 <- a %>% 
+    filter(est_state == 2)
+  
+  # Determine the estimated state at the last time step
+  final_state <- pull(a[nrow(a), "est_state"])
+  
+  # Extract estimated transition matrix
+  est_P <- t(matrix(getpars(fit_mod1)[3:6], nrow = 2, ncol = 2))
+  
+  # predict states for forecast years
+  future_state <- tail(run.pred.mc.sim(est_P, num.iters = 2, final_state), n = 1)
+  
+  rec_preds <- rep(NA, nsims)  
+  for(i in 1:nsims){
+    rec_preds[i] <- pred.rec(future_state, mu1 = mean(est_state_1$logRS), mu2 = mean(est_state_2$logRS),
+                             sd1 = sd(est_state_1$logRS), sd2 = sd(est_state_2$logRS),df2, x)
+  }  
+  
+  # return forecasts
+  return(rec_preds)
+  
+}
+
+rec_chpt_sample <- function(x, df1, nsims){
   # subset data to window size
   dat <- df1[1:(x-1)]
+  
+  # fit changepoints
+  fitPelt <- cpt.meanvar(log(dat),method="PELT",test.stat="Normal",penalty="Manual",
+                         pen.value = "2*(diffparam+1)*(n/(n-diffparam-2))", minseglen=7)
+  changes	<- fitPelt@cpts
+  
+  # use estimates of mean and variance of final regime to predict next year
+  mu <- tail(fitPelt@param.est$mean, n = 1)
+  sigmaR <- sqrt(tail(fitPelt@param.est$variance, n = 1))
+  pred <- exp(mu + rnorm(nsims, 0, sigmaR))
+  
+  # return forecasts
+  return(pred)
+}
+
+
+# Long-term forecast functions -------------------------------------------------------------
+# x = test set vector, df1 = recruitment time series, df2 = spawning biomass time series, 
+# nsims = number of simulations
+
+lrec_mean <- function(x, df1, nsims){
+  # subset data to window size
+  dat <- df1[1:(x-5)]
   
   # calculate mean and standard deviation of time series window
   mu <- mean(log(dat))
@@ -212,29 +224,30 @@ lrec_mean <- function(x, df1){
   sigmaR <- sd(log(dat))
   
   # calculate prediction
-  preds <- exp(mu + rnorm(5, 0, sigmaR))
+  preds <- exp(mu + rnorm(nsims, 0, sigmaR))
   return(preds)
 }
 
-lrec_AR <- function(x, df1){
+lrec_AR <- function(x, df1, nsims){
   
   # subset data to window size
-  dat <- df1[1:(x-1)]
+  dat <- df1[1:(x-5)]
   
   # fit AR model, will use sd estimate from model estimates
   mod <- Arima(log(dat), order = c(1,0,0))
   sigmaR <- sqrt(mod$sigma2)
   
   # calculate prediction
-  preds <- exp((forecast(mod, h = 5)$mean)[1:5] + rnorm(5, 0, sigmaR))
+  preds <- exp(forecast(mod, h = 5)$mean[5] + rnorm(nsims, 0, sigmaR))
+  
   return(preds)
 }
 
-lrec_BH <- function(x, df1, df2){
+lrec_BH <- function(x, df1, df2, nsims){
   # subset spawning and recruit data to window size
-  datr <- df1[1:(x-1)]
-  dats <- df2[1:(x-1)]
-  s_x <- df2[x:(x+4)]
+  datr <- df1[1:(x-5)]
+  dats <- df2[1:(x-5)]
+  s_x <- df2[x]
   
   # Likelihood function
   BHminusLL <- function(loga, logb, logsigmaR){
@@ -258,26 +271,26 @@ lrec_BH <- function(x, df1, df2){
   b <- exp(coef(mle_out)[2])
   sigmaR <- exp(coef(mle_out)[3])
   
-  # predict one step ahead
-  five_ahead <- a*s_x/(1 + b*s_x)*exp(rnorm(5,0,sigmaR))
+  # predict
+  five_ahead <- a*s_x/(1 + b*s_x)*exp(rnorm(nsims,0,sigmaR))
   
   return(unname(five_ahead))
 }
 
-lrec_HMM_sample <- function(x, df1, df2){
+lrec_HMM_sample <- function(x, df1, df2, nsims){
   # subset spawning and recruit data to window size
-  datr <- df1[1:(x-1)]
-  dats <- df2[1:(x-1)]
+  datr <- df1[1:(x-5)]
+  dats <- df2[1:(x-5)]
   
   # make a data frame for depmix package
   a <- tibble(rec = datr,
               spawn = dats,
               logRS = log(datr/dats))
   # fit hidden markov model
-  mod <- depmix(logRS ~ spawn, data = a, nstates = 2, family = gaussian())
+  mod <- depmix(logRS ~ spawn, data = a, nstates = 2, family = gaussian())#, instart = c(0.9, 0.1))
   fit_mod <- fit(mod)
   #summary(fit_mod)
-  fit_post <- posterior(fit_mod)
+  fit_post <- posterior(fit_mod, type = "viterbi")
   
   # update data frame with posterior state classification
   a <- a %>% 
@@ -294,21 +307,23 @@ lrec_HMM_sample <- function(x, df1, df2){
   # Extract estimated transition matrix
   est_P <- t(matrix(getpars(fit_mod)[3:6], nrow = 2, ncol = 2))
   
-  # predict states for forecast years
-  future_states <- run.pred.mc.sim(est_P, num.iters = 6, final_state)
+  # predict states for forecast years, only save state for terminal year
+  future_state <- tail(run.pred.mc.sim(est_P, num.iters = 6, final_state), n = 1)
   
-  # forecast recruitment
-  rec_preds <- pred.rec(future_states, mu1 = mean(est_state_1$rec), mu2 = mean(est_state_2$rec),
-                        sd1 = sd(est_state_1$rec), sd2 = sd(est_state_2$rec))
+  rec_preds <- rep(NA, nsims)  
+  for(i in 1:nsims){
+    rec_preds[i] <- pred.rec(future_state, mu1 = mean(est_state_1$logRS), mu2 = mean(est_state_2$logRS),
+                             sd1 = sd(est_state_1$logRS), sd2 = sd(est_state_2$logRS),df2, x)
+  }  
   
   # return forecasts
-  return(rec_preds[-1])
+  return(rec_preds)
 }
 
-lrec_simplex <- function(x, df1){
+lrec_simplex <- function(x, df1, nsims){
   
-  sim_lib <- c(1, x-1)
-  sim_pred <- c(x-1, length(df1))
+  sim_lib <- c(1, x-5)
+  sim_pred <- c(x-5, length(df1))
   
   # calculate standard deviation for subsetted data frame
   dat <- df1[1:(x-1)]
@@ -324,18 +339,39 @@ lrec_simplex <- function(x, df1){
   simplex_output2 <- simplex(log(df1), sim_lib, sim_pred, E = E_val, stats_only = FALSE)
   
   preds <- na.omit(simplex_output2$model_output[[1]])
-  
+  pred_row <- which(preds$Index == x)
   # return forecast
-  pred <- exp(preds[1:5, 3] + rnorm(5, 0, sigmaR))
+  pred <- exp(preds[pred_row, 3] + rnorm(nsims, 0, sigmaR))
   return(pred)
 }
+
+lrec_chpt_sample <- function(x, df1, nsims){
+  # subset data to window size
+  dat <- df1[1:(x-5)]
+  
+  # fit changepoints
+  fitPelt <- cpt.meanvar(log(dat),method="PELT",test.stat="Normal",penalty="Manual",
+                         pen.value = "2*(diffparam+1)*(n/(n-diffparam-2))", minseglen=7)
+  changes	<- fitPelt@cpts
+  
+  # use estimates of mean and variance of final regime to predict next year
+  mu <- tail(fitPelt@param.est$mean, n = 1)
+  sigmaR <- sqrt(tail(fitPelt@param.est$variance, n = 1))
+  pred <- exp(mu + rnorm(nsims, 0, sigmaR))
+  
+  # return forecasts
+  return(pred)
+}
+
+
 # Projection Functions -----------------------------------------------------
 # A function that passes in the data files and calculates 1-step forecasts for an 
 # expanding window. For each simulation, the function returns a matrix of the observed 
 # recruitment and 1-step (or 5-step) ahead forecasts for each of the methods above at time t in 
 # the test set.
-
-# nsims = # of simulations with the given test set of recruits and sbiomass
+# fmethods = vector of which forcasting methods to use ("m" = mean, "ar" = AR(1), "bh" = Beverton-Holt,
+# "hmm" = hidden Markov model sampling, "simplex" = simplex projection, "chpt" = PELT change point sampling)
+# nsims = number of simulations
 # time_vec = vector of test data for 1 step ahead forecasts
 # recruits = recruitment time series
 # sbiomass = spawning biomass time series
@@ -351,27 +387,25 @@ expanding_window <- function(fmethods, nsims, time_vec, recruits, sbiomass){
     # first column is observed recruitment
     preds[,1] <- recruits[time_vec]
     
-    for(j in 2:(nsims+1)){
-      print((j-1))
-      for(k in 1:length(time_vec)){
-        preds[k, j] <- switch(
-          fmethod,
-          "m" = rec_mean(time_vec[k], recruits),
-          "ar" = rec_AR(time_vec[k], recruits),
-          "bh" = rec_BH(time_vec[k], recruits, sbiomass),
-          "hmm" = rec_HMM_sample(time_vec[k],recruits, sbiomass),
-          "simplex" = rec_simplex(time_vec[k], recruits))
-      }
-      
+    for(k in 1:length(time_vec)){
+      preds[k, 2:(nsims+1)] <- switch(
+        fmethod,
+        "m" = rec_mean(time_vec[k], recruits, nsims),
+        "ar" = rec_AR(time_vec[k], recruits, nsims),
+        "bh" = rec_BH(time_vec[k], recruits, sbiomass, nsims),
+        "hmm" = rec_HMM_sample(time_vec[k],recruits, sbiomass, nsims),
+        "simplex" = rec_simplex(time_vec[k], recruits, nsims),
+        "chpt" = rec_chpt_sample(time_vec[k], recruits, nsims))
     }
     
     sim_preds[,,i] <- preds
   }
-
+  
   return(sim_preds)
 }
 
-expanding_window_5yr <- function(fmethods, nsims, time_vec, time_vec2, recruits, sbiomass){
+
+expanding_window_5yr <- function(fmethods, nsims, time_vec, recruits, sbiomass){
   
   sim_preds <- array(NA, dim = c(length(time_vec), (nsims+1), length(fmethods)))
   
@@ -382,30 +416,15 @@ expanding_window_5yr <- function(fmethods, nsims, time_vec, time_vec2, recruits,
     # first column is observed recruitment
     preds[,1] <- recruits[time_vec]
     
-    for(j in 2:(nsims+1)){
-      # create a matrix for each 5 year expanding window sim
-      raw_preds <- matrix(data = NA, nrow = length(time_vec), ncol = length(time_vec2))
-      for(k in 1:length(time_vec2)){
-        
-        if(fmethod == "m"){
-          raw_preds[k:(k+4), k] <- lrec_mean(time_vec2[k], recruits)
-        }else if(fmethod == "ar"){
-          raw_preds[k:(k+4), k] <- lrec_AR(time_vec2[k], recruits)
-        }else if(fmethod == "bh"){
-          raw_preds[k:(k+4), k] <- lrec_BH(time_vec2[k], recruits, sbiomass)
-        }else if(fmethod == "hmm"){
-          raw_preds[k:(k+4), k] <- lrec_HMM_sample(time_vec2[k],recruits, sbiomass)
-        }else {
-          raw_preds[k:(k+4), k] <- lrec_simplex(time_vec2[k], recruits)
-        }
-        
-      }
-      #print(raw_preds)
-      # take mean of overlapping predictions, save simulation to sim dataframe
-      preds[,j] <- apply(raw_preds, 1, median, na.rm = TRUE)
-      
-      # print out number of sims completed
-      print(j)
+    for(k in 1:length(time_vec)){
+      preds[k, 2:(nsims+1)] <- switch(
+        fmethod,
+        "m" = lrec_mean(time_vec[k], recruits, nsims),
+        "ar" = lrec_AR(time_vec[k], recruits, nsims),
+        "bh" = lrec_BH(time_vec[k], recruits, sbiomass, nsims),
+        "hmm" = lrec_HMM_sample(time_vec[k],recruits, sbiomass, nsims),
+        "simplex" = lrec_simplex(time_vec[k], recruits, nsims),
+        "chpt" = lrec_chpt_sample(time_vec[k], recruits, nsims))
     }
     
     sim_preds[,,i] <- preds
@@ -413,6 +432,7 @@ expanding_window_5yr <- function(fmethods, nsims, time_vec, time_vec2, recruits,
   
   return(sim_preds)
 }
+
 
 
 # Performance Stat Functions ----------------------------------------------
